c34170f7378c567d87f1ff93a09b3d788ddc60ac
albertrein <guilhermealberto8@gmail.com>
2018-11-21 14:50:45 -0200

    Um voto deve estar vinculado a um eleitor e a uma eleição válida. [FALTA TESTAR]

--- src/main/java/br/edu/ulbra/election/election/repository/VoteRepository.java
+++ src/main/java/br/edu/ulbra/election/election/repository/VoteRepository.java
@@ -1,7 +1,10 @@
 package br.edu.ulbra.election.election.repository;
 
+import br.edu.ulbra.election.election.model.Election;
 import br.edu.ulbra.election.election.model.Vote;
+import br.edu.ulbra.election.election.output.v1.VoterOutput;
 import org.springframework.data.repository.CrudRepository;
 
 public interface VoteRepository extends CrudRepository<Vote, Long> {
+    Vote getVoteByElection(Election election);
 }
--- src/main/java/br/edu/ulbra/election/election/service/VoteService.java
+++ src/main/java/br/edu/ulbra/election/election/service/VoteService.java
@@ -1,12 +1,15 @@
 package br.edu.ulbra.election.election.service;
 
+import br.edu.ulbra.election.election.client.VoterClientService;
 import br.edu.ulbra.election.election.exception.GenericOutputException;
 import br.edu.ulbra.election.election.input.v1.VoteInput;
 import br.edu.ulbra.election.election.model.Election;
 import br.edu.ulbra.election.election.model.Vote;
 import br.edu.ulbra.election.election.output.v1.GenericOutput;
+import br.edu.ulbra.election.election.output.v1.VoterOutput;
 import br.edu.ulbra.election.election.repository.ElectionRepository;
 import br.edu.ulbra.election.election.repository.VoteRepository;
+import feign.FeignException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,11 +21,12 @@ public class VoteService {
     private final VoteRepository voteRepository;
 
     private final ElectionRepository electionRepository;
-
+    private final VoterClientService voterClientService;
     @Autowired
-    public VoteService(VoteRepository voteRepository, ElectionRepository electionRepository){
+    public VoteService(VoteRepository voteRepository, ElectionRepository electionRepository, VoterClientService voterClientService){
         this.voteRepository = voteRepository;
         this.electionRepository = electionRepository;
+        this.voterClientService = voterClientService;
     }
 
     public GenericOutput electionVote(VoteInput voteInput){
@@ -61,7 +65,24 @@ public class VoteService {
         if (voteInput.getVoterId() == null){
             throw new GenericOutputException("Invalid Voter");
         }
+
         // TODO: Validate voter
+        try{ // Validanting voter, if voter exists
+            VoterOutput voterOutput = voterClientService.getById(voteInput.getVoterId());
+            if(voterOutput == null){
+                throw new GenericOutputException("Invalid Voter");
+            }
+        } catch (FeignException e){
+            if (e.status() == 500) {
+                throw new GenericOutputException("Invalid Voter");
+            }
+        }
+
+
+        if(electionRepository.getElectionById(voteInput.getElectionId()) == null){ //Retornando uma eleição pelo id, verificando se uma elição existe
+            throw new GenericOutputException("Invalid Election");
+        }
+
 
         return election;
     }
