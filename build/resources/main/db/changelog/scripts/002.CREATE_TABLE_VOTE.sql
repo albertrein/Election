2fc936e0487b942207bf04050bf9fab78fb65bb9
albertrein <guilhermealberto8@gmail.com>
2018-11-21 10:57:32 -0200

    ALterando repositório e iniciando criação VoterClient

--- build/resources/main/db/changelog/scripts/002.CREATE_TABLE_VOTE.sql
+++ build/resources/main/db/changelog/scripts/002.CREATE_TABLE_VOTE.sql
@@ -2,5 +2,7 @@ create table vote (
   id integer identity primary key,
   election_id integer not null,
   voter_id integer not null,
-  candidate_number integer not null
+  candidate_id integer,
+  blank_vote boolean not null,
+  null_vote boolean not null
 );
