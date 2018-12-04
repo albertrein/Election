77fdde49d3c2e662083932517667b8b2d870e945
albertrein <guilhermealberto8@gmail.com>
2018-12-04 00:46:39 -0200

    Corrigindo a listade estados do país.

--- src/main/java/br/edu/ulbra/election/election/validations/ValidateStateCode.java
+++ src/main/java/br/edu/ulbra/election/election/validations/ValidateStateCode.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 import java.util.List;
 
 public class ValidateStateCode {
-    private static final List<String> brStates = Arrays.asList("AC","AL","AP","AM","BA","CE","DF","ES","GO","MA","MT","MS","MG","PA","PB","PR","PE","PI","RJ","RN","RS","RO","RR","SC","SP","SE","TO");
+    private static final List<String> brStates = Arrays.asList("AC","AL","AP","AM","BA","BR","CE","DF","ES","GO","MA","MT","MS","MG","PA","PB","PR","PE","PI","RJ","RN","RS","RO","RR","SC","SP","SE","TO");
 
     public ValidateStateCode(){}
 
